38e96596dfed1bf709e29012de3f88c83533a421
Дмитро Недєв <dmytro.nedyev@o365.opu.ua>
2024-11-01 11:44:27 +0200

    added browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 
 const scss = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
 
 function styles() {
   return src('./app/scss/main.scss')
     .pipe(scss())
-    .pipe(dest('./app/css'));
+    .pipe(dest('./app/css'))
+    .pipe(browserSync.stream());
 }
 
 function watchProject() {
   watch(['app/scss/**/*.scss'], styles);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
+}
+
+function initBrowserSync() {
+  browserSync.init({
+    server: './app',
+  });
 }
 
 exports.styles = styles;
 exports.watchProject = watchProject;
+
+exports.default = parallel(styles, initBrowserSync, watchProject);
